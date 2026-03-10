f435c26dc3505e88e727580d9b32ebc4b35c7d56
LogicMasterBV <bajramvisha@hotmail.com>
2026-03-10 00:37:52 +0000

    Adds background color to login page and hover effect to navbar buttons.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,41 @@
-import { Component, signal } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./shared/navbar/navbar";
-import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, CommonModule,
-    HttpClientModule],
+  imports: [RouterOutlet, NavbarComponent, CommonModule, HttpClientModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('cinedoro-frontend');
+
+  showSplash = false;
+  fadeOut = false;
+
+  constructor(private cdr: ChangeDetectorRef) {
+
+    const splashShown = sessionStorage.getItem('splashShown');
+
+    if (!splashShown) {
+
+      this.showSplash = true;
+
+      setTimeout(() => {
+        this.fadeOut = true;
+        this.cdr.detectChanges();
+      }, 2600);
+
+      setTimeout(() => {
+        this.showSplash = false;
+        sessionStorage.setItem('splashShown', 'true');
+        this.cdr.detectChanges();
+      }, 3400);
+
+    }
+
+  }
 }
